3b6de6673fabb3b53d9f6c1692f10579643ae3a1
darkheart101 <kurokokoro@windowslive.com>
2019-10-07 21:50:19 +0300

    post requests

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -6,9 +6,15 @@ use Framework\View;
 
 class TestController
 {
-    public function test()
+    public function test_get()
     {
         $view = new View;
         return $view->view('test.view.php', ['test'=> 'test']);
+    }
+
+    public function test_post()
+    {
+        $view = new View;
+        return $view->view('test_post.view.php', ['post'=> $_POST]);
     }
 }
---
+++ app/Views/test_post.view.php
@@ -0,0 +1,6 @@
+<?php
+
+foreach($view_variables['post'] as $post)
+{
+    echo "<h1>".$post."</h1>";
+}
