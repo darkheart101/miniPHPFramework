1804010d068414e0abf858df51263ae61e9a361e
darkheart101 <kurokokoro@windowslive.com>
2019-10-09 20:53:02 +0300

    additional http methods

--- framework/Route.php
+++ framework/Route.php
@@ -5,20 +5,15 @@ namespace Framework;
 class Route{
     protected $routing = array();
 
-    public function __construct()
-    {
-
-    }
 
     public function get( $url , $controller, $method )
     {
 
-        array_push(
-                    $this->routing, array(
-                                    'http_method'=>'GET',
-                                    'url'=>$url,
-                                    'controller'=> $controller,
-                                    'method' => $method
+        array_push( $this->routing,
+                    array('http_method'=>'GET',
+                          'url'=>$url,
+                          'controller'=> $controller,
+                          'method' => $method
                     )
                   );
     }
@@ -26,12 +21,36 @@ class Route{
     public function post( $url , $controller, $method )
     {
 
-        array_push(
-                    $this->routing, array(
-                                    'http_method'=>'POST',
-                                    'url'=>$url,
-                                    'controller'=> $controller,
-                                    'method' => $method
+        array_push( $this->routing,
+                    array('http_method'=>'POST',
+                          'url'=>$url,
+                          'controller'=> $controller,
+                          'method' => $method
+                    )
+                  );
+    }
+
+
+    public function delete( $url , $controller, $method )
+    {
+
+        array_push( $this->routing,
+                    array('http_method'=>'DELETE',
+                          'url'=>$url,
+                          'controller'=> $controller,
+                          'method' => $method
+                    )
+                  );
+    }
+
+    public function put( $url , $controller, $method )
+    {
+
+        array_push( $this->routing,
+                    array('http_method'=>'PUT',
+                          'url'=>$url,
+                          'controller'=> $controller,
+                          'method' => $method
                     )
                   );
     }
